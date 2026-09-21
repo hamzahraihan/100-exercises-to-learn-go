7cc88f6e481d9655c4735af285b87c02185bcbd0
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2026-09-21 11:29:06 +0700

    solution: 07_concurrency wave 1

--- exercises/07_concurrency/03_buffered/collect.go
+++ exercises/07_concurrency/03_buffered/collect.go
@@ -3,7 +3,18 @@ package collect
 
 // Collect returns [0, 2, ..., 2*(n-1)] computed by n goroutines sending
 // over a buffered channel, collected in order.
-// TODO: make(chan int, n), one goroutine per i sending 2*i, collect all.
 func Collect(n int) []int {
-	return make([]int, n)
+	type pair struct {
+		i, v int
+	}
+	ch := make(chan pair, n)
+	for i := 0; i < n; i++ {
+		go func(i int) { ch <- pair{i, 2 * i} }(i)
+	}
+	out := make([]int, n)
+	for k := 0; k < n; k++ {
+		p := <-ch
+		out[p.i] = p.v
+	}
+	return out
 }
--- exercises/07_concurrency/04_select/fetch.go
+++ exercises/07_concurrency/04_select/fetch.go
@@ -1,11 +1,17 @@
 // Package fetch teaches select with timeouts.
 package fetch
 
-import "time"
+import (
+	"errors"
+	"time"
+)
 
 // Fetch simulates a fetch taking d. It must fail when d exceeds 200ms.
-// TODO: select on time.After(d) for success vs time.After(200ms) for timeout.
 func Fetch(d time.Duration) (string, error) {
-	<-time.After(d)
-	return "data", nil
+	select {
+	case <-time.After(d):
+		return "data", nil
+	case <-time.After(200 * time.Millisecond):
+		return "", errors.New("fetch timed out")
+	}
 }
