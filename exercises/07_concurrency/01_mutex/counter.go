eb9c281f89191a84e7a6d9db3358ecbd03b90801
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2026-09-21 10:26:42 +0700

    solution: 07_concurrency

--- exercises/07_concurrency/01_mutex/counter.go
+++ exercises/07_concurrency/01_mutex/counter.go
@@ -4,7 +4,6 @@ package counter
 import "sync"
 
 // Counter is a goroutine-safe int.
-// TODO: add a sync.Mutex field and guard n.
 type Counter struct {
 	mu sync.Mutex
 	n  int
@@ -12,10 +11,14 @@ type Counter struct {
 
 // Add increments by n.
 func (c *Counter) Add(n int) {
-	c.n += n // TODO: Lock/Unlock
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	c.n += n
 }
 
 // Value returns the current count.
 func (c *Counter) Value() int {
-	return c.n // TODO: Lock/Unlock
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	return c.n
 }
--- exercises/07_concurrency/02_channels/pipeline.go
+++ exercises/07_concurrency/02_channels/pipeline.go
@@ -3,9 +3,21 @@ package pipeline
 
 // DoubleAll doubles each number using a goroutine and channel,
 // preserving input order.
-// TODO: launch a goroutine per index (or one worker), send results
-// over a channel, collect in order.
 func DoubleAll(nums []int) []int {
 	out := make([]int, len(nums))
+	type result struct {
+		idx int
+		val int
+	}
+	ch := make(chan result, len(nums))
+	for i, n := range nums {
+		go func(i, n int) {
+			ch <- result{idx: i, val: n * 2}
+		}(i, n)
+	}
+	for range nums {
+		r := <-ch
+		out[r.idx] = r.val
+	}
 	return out
 }
